37b7e0da1ad58389d566c0fbb5d19d4b4f270b27
pito <crisvitoc@gmail.com>
2024-01-25 09:15:29 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,7 @@
                                     <p class="lead text-body-secondary">Something short and leading about the collection
                                         below—its
                                         contents, the creator, etc. Make it short and sweet, but not too short so folks
-                                        don’t simply
-                                        skip over it entirely.</p>
+                                        don’t simply</p>
                                 </div>
                             </div>
                         </section>
